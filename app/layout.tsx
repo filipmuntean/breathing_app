8e7b737b575499a137c825bbb9251901293acafa
Filip Muntean <f.muntean@student.vu.nl>
2025-03-05 21:57:31 +0100

    I think adsense should owkr now

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import config from "@/config";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import Script from "next/script";
+import Head from "next/head";
 const font = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
@@ -23,10 +24,31 @@ export const metadata = getSEOTags();
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-theme={config.colors.theme} className={font.className}>
+      <head>
+
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1660907750605458"
+     crossOrigin="anonymous"></Script>
+      </head>
       <body>
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1660907750605458"
      crossOrigin="anonymous"></Script>
+
+  <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1660907750605458"
+      crossOrigin="anonymous"></Script>
+  <ins className="adsbygoogle"
+      style={{ 
+        display: 'block', 
+        // You can adjust width and height if needed
+        width: '100%' 
+      }}
+      data-ad-client="ca-pub-1660907750605458"
+      data-ad-slot="4225636729"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+<Script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</Script>
         <ClientLayout>{children}</ClientLayout>
         <Analytics />
       </body>
